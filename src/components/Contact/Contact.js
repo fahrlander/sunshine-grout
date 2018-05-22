99fc25f9dce2d59f9aaec2b14c048090f1b7d310
Ricardo De la Fuente <rdelafuente350@gmail.com>
2018-05-23 02:32:06 +0900

    fixing the contact-form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,13 +3,8 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <form className="form box-shadow" name="contact" method="POST"
-      netlify netlify-honeypot="bot-field">
-      <p class="hidden">
-        <label>Don’t fill this out if you're human:
-          <input name="bot-field" />
-        </label>
-      </p>
+    <form className="form box-shadow" name="contact"
+      netlify="true">
       <div className="form-group">
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" required placeholder="John Doe" />
@@ -30,9 +25,7 @@ const Contact = () => {
         <label htmlFor="message">Message:</label>
         <textarea id="message" required placeholder="Type your message..." />
       </div>
-
-      <div netlify-recaptcha></div>
-
+      
       <div className="form-group">
         <button className="pull-right btn btn-primary" type="submit">
           Submit
